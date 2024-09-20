c8160eab81bf6954e84ade24839657fc41567a4d
Joosef Abokther <49030615+Jouchef@users.noreply.github.com>
2024-09-20 17:28:05 +0300

    Debug Vite specific variable names

--- client/src/services/pinService.tsx
+++ client/src/services/pinService.tsx
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-// const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000'
+// const apiUrl = process.env.VITE_API_URL || 'http://localhost:3000'
 
-const apiUrl = process.env.REACT_APP_API_URL 
+const apiUrl = process.env.VITE_API_URL 
 
 export const getAll = async () => {
   return await axios.get(`${apiUrl}/api/coordinates`)
