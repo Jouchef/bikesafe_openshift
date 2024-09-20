8c25532b7ba4c4e4ed1ac5e3ae8daf3874793820
Joosef Abokther <49030615+Jouchef@users.noreply.github.com>
2024-09-20 17:37:12 +0300

    process to import metaenv

--- client/src/services/pinService.tsx
+++ client/src/services/pinService.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 // const apiUrl = process.env.VITE_API_URL || 'http://localhost:3000'
 
-const apiUrl = process.env.VITE_API_URL 
+const apiUrl = import.meta.env.VITE_API_URL 
 
 export const getAll = async () => {
   return await axios.get(`${apiUrl}/api/coordinates`)
--- client/src/services/theftService.tsx
+++ client/src/services/theftService.tsx
@@ -3,7 +3,7 @@ import { LatLng } from "leaflet"
 
 // const apiUrl = process.env.VITE_API_URL || 'http://localhost:3000'
 
-const apiUrl = process.env.VITE_API_URL 
+const apiUrl = import.meta.env.VITE_API_URL 
 
 export const sendTheftReport = async (coordinates: LatLng) => {
   const res = await axios.post(`${apiUrl}/api/coordinates`, coordinates)
