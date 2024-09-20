85bf5a60c29665cc18d1e0cad5c9da44810845e3
Joosef Abokther <49030615+Jouchef@users.noreply.github.com>
2024-09-20 18:24:58 +0300

    debug

--- client/src/services/pinService.tsx
+++ client/src/services/pinService.tsx
@@ -1,10 +1,9 @@
 import axios from "axios"
 
-// const apiUrl = process.env.VITE_API_URL || 'http://localhost:3000'
-
-console.log("PIN_VITE_API_URL:", import.meta.env.VITE_API_URL)
 
 const apiUrl = import.meta.env.VITE_API_URL 
+
+console.log("PIN_VITE_API_URL:", import.meta.env.VITE_API_URL)
 
 export const getAll = async () => {
   return await axios.get(`${apiUrl}/api/coordinates`)
--- client/src/services/theftService.tsx
+++ client/src/services/theftService.tsx
@@ -1,11 +1,10 @@
 import axios from "axios"
 import { LatLng } from "leaflet"
 
-// const apiUrl = process.env.VITE_API_URL || 'http://localhost:3000'
 
+const apiUrl = import.meta.env.VITE_API_URL
+ 
 console.log("THEFT_VITE_API_URL:", import.meta.env.VITE_API_URL)
-
-const apiUrl = import.meta.env.VITE_API_URL 
 
 export const sendTheftReport = async (coordinates: LatLng) => {
   const res = await axios.post(`${apiUrl}/api/coordinates`, coordinates)
