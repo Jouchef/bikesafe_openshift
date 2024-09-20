fca5327f38cf345a7446f0672fb90dce95c65148
Joosef Abokther <49030615+Jouchef@users.noreply.github.com>
2024-09-20 17:05:17 +0300

    remove localhost mentions for debugging

--- client/src/services/theftService.tsx
+++ client/src/services/theftService.tsx
@@ -1,7 +1,9 @@
 import axios from "axios"
 import { LatLng } from "leaflet"
 
-const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000'
+// const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3000'
+
+const apiUrl = process.env.REACT_APP_API_URL 
 
 export const sendTheftReport = async (coordinates: LatLng) => {
   const res = await axios.post(`${apiUrl}/api/coordinates`, coordinates)
